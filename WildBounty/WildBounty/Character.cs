8e7f25ecf541e87bb1ed7f4dc1746f31011cbf12
Dezmon Gilbert <dog6487@rit.edu>
2016-02-29 02:32:05 -0500

    Simple parent classes for Characters and Weapons. Not much in them at the moment

---
+++ WildBounty/WildBounty/Character.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WildBounty
+{
+    /*
+     * Authors: Dezmon Gilbert
+     * Purpose: Parent class for player and enemies
+     * Caveats: none
+     * */
+    class Character
+    {
+        // attributs
+        private int health; // health of character
+        
+        // properties 
+        public int Health
+        {
+            get { return health; }
+        }
+    }
+}
---
+++ WildBounty/WildBounty/Weapon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WildBounty
+{
+    /*
+     * Authors: Dezmon Gilbert
+     * Purpose: Parent class for weapons
+     * Caveats: none
+     * */
+    class Weapon
+    {
+        // attributes
+        private int damage; // damage dealt
+
+        // properties 
+        public int Damage
+        {
+            get { return damage; }
+        }
+    }
+}
+

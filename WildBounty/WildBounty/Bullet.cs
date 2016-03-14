883e01b720864bc713d9ea5f15afe87316cd71c4
Niko Bazos <ndb9897@rit.edu>
2016-03-14 01:33:46 -0400

    Update for the Bullet class. Added algorithim and method to how collision with bullets will work.

--- WildBounty/WildBounty/Bullet.cs
+++ WildBounty/WildBounty/Bullet.cs
@@ -7,21 +7,33 @@ namespace WildBounty
 {
     class Bullet : Character
     {
+        // Author: Niko Bazos
+        // Purpose: Bullet Class
+        // Date: 3/15/16
         // Collision rectangles 
-
+        Rectangle bCollision;
 
         // @param Character object
         // Check to see if rectangles intersect with a character object
-            
+        public void Collision(GameObject obj)
+        {
+            // Maybe just subtract the health from a general GameObject value without specifying
+            if(bCollision.Intersects(obj.Rec) // Property for game object rectangle
+            {
+                // Use property and subtract 10 health from that
+            }
+
+            // Not using:
             // if bullet intersects with Enemy 
-                // kill enemy
+            // kill enemy
 
             // if bullet intersects with Character
-                // Bring down health
+            // Bring down health
 
             // if it hits an obstacle
-                // Bring down health of the object
+            // Bring down health of the object
+        }
 
-        // Maybe just subtract the health from a general GameObject value without specifying
+        
     }
 }
